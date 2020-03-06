c6d379ae5a4522bb2b42930fa0965fe2da6060f0
Heroponne <florence.orgeret@gmail.com>
2020-03-06 22:41:08 +0100

    [BACK] added route for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
   resources :favorites
 
   resources :comments
 
   resources :internships
-  root to: "users#index"
+  root to: "static_pages#home"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
