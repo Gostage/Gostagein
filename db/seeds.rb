aa3505c875fba6645b31c5732b8186c56404ffce
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-03-09 13:39:56 +0100

    [doing] back seed user and internship

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#User.destroy_all
+#Internship.destroy_all
+# Comment.destroy_all
+# Favorite.destroy_all
+
+
+require 'faker'
+
+100.times do |user|
+	user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "azerty")
+end
+
+puts "100 users ont été crées"
+
+=begin
+100.times do |internship|
+	internship = Internship.create(adress: Faker::Address.street_address, 
+		zipcode: Faker::Address.zip_code, 
+		city: Faker::Address.city, 
+		specialty: ["Hospitalier", "Maison médicale","Social", "Médico-social", "Petite enfance", "Associatif", "Scolaire", "Médico-pédagogique", "Libéral", "Recherche", "Entreprise","Privé", "Justice","Police Nationale", "Armée", "Carcéral", "Insertion professionnelle", "Recrutement", "Gestion de carrières", "Ressouces Humaines", "Marketing", "Santé au travail", "Autres"].sample, 
+		organization: Faker::Movies::HarryPotter.location, 
+		population: ["Personnes âgées", "Enfants", "Adolescents", "Adultes", "Autistes", "Haut Potentionel", "Personnes Cérébro-lésées", "Autres"].sample, 
+		notation: Faker::Number.decimal(l_digits: 1, r_digits: 1), 
+		user: User.sample.all , 
+		description: Faker::GreekPhilosophers.quote, 
+		title: Faker::Job.title, 
+		cursus:["Licence 1", "Licence 2", "Licence 3", "Master 1", "Master 2"].sample)
+end
+
+puts "100 internships ont été crées"
+=end
