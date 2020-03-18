75351f69c79871702cbf9c8cb1e457eeb37bcc2d
Heroponne <florence.orgeret@gmail.com>
2020-03-18 16:41:12 +0100

    [BACK] removed useless routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   root to: "static_pages#home"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only: [:show]
 end
