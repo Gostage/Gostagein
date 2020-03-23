353c776963eabf0e37b1ce00ef89d8a331d6a457
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-03-23 15:52:13 +0100

    [done] new attribut duration to internship

--- db/migrate/20200305164442_add_columns_to_internships.rb
+++ db/migrate/20200305164442_add_columns_to_internships.rb
@@ -2,5 +2,6 @@ class AddColumnsToInternships < ActiveRecord::Migration[5.2]
   def change
     add_column :internships, :title, :string
     add_column :internships, :cursus, :string
+    add_column :internships, :duration, :integer
   end
 end
