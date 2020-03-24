95a90e488933b3f1cf14a9dc79bc79c5c822b58d
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-03-24 17:20:32 +0100

    [done] remove duration to make a new migration

--- db/migrate/20200305164442_add_columns_to_internships.rb
+++ db/migrate/20200305164442_add_columns_to_internships.rb
@@ -2,6 +2,5 @@ class AddColumnsToInternships < ActiveRecord::Migration[5.2]
   def change
     add_column :internships, :title, :string
     add_column :internships, :cursus, :string
-    add_column :internships, :duration, :float
   end
 end
