1e2bbb8dc3aa8ded1e3b5f2d4bb68d2614357f09
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-03-30 15:46:22 +0200

    new migration for new association user review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_130020) do
+ActiveRecord::Schema.define(version: 2020_03_30_134401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,9 @@ ActiveRecord::Schema.define(version: 2020_03_30_130020) do
     t.bigint "internship_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["internship_id"], name: "index_reviews_on_internship_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -80,4 +82,5 @@ ActiveRecord::Schema.define(version: 2020_03_30_130020) do
   add_foreign_key "favorites", "users"
   add_foreign_key "internships", "users"
   add_foreign_key "reviews", "internships"
+  add_foreign_key "reviews", "users"
 end
