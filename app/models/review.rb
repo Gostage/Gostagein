8bbc941bf261486c9a0754cb73ecb78f0eaf68a6
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-03-30 15:07:54 +0200

    validates and association review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,15 @@
 class Review < ApplicationRecord
+  validates :title,
+    presence: true,
+    length: { in: 10..150}
+  validates :description,
+    presence: true,
+    length: { in: 30..1000 }
+  validates :notation,
+    presence: true,
+    inclusion: { in: 1..6},
+    format: { with: /\A\d+\.*\d{0,1}\z/ }
+
   belongs_to :internship
+
 end
