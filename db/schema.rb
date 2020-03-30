0013292c6027800665b8f7bb5d8338154794c841
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-03-30 16:10:41 +0200

    new migration for attribut salary in internships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_134401) do
+ActiveRecord::Schema.define(version: 2020_03_30_140529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_134401) do
     t.string "cursus"
     t.float "duration"
     t.string "region"
+    t.float "salary"
     t.index ["user_id"], name: "index_internships_on_user_id"
   end
 
