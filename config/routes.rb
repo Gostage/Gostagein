b0e191424f51eb573ad3293f87d9a886fd10f823
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-03-30 15:02:15 +0200

    new scaffold Review

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :reviews
   get 'static_pages/home'
   resources :favorites
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_123933) do
+ActiveRecord::Schema.define(version: 2020_03_30_130020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(version: 2020_03_30_123933) do
     t.index ["user_id"], name: "index_internships_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.float "notation"
+    t.text "description"
+    t.bigint "internship_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["internship_id"], name: "index_reviews_on_internship_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -69,4 +79,5 @@ ActiveRecord::Schema.define(version: 2020_03_30_123933) do
   add_foreign_key "favorites", "internships"
   add_foreign_key "favorites", "users"
   add_foreign_key "internships", "users"
+  add_foreign_key "reviews", "internships"
 end
