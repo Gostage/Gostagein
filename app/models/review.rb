1b3dd8fc2042c8579817826d63ebce01aba7ffc2
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-03-30 15:14:38 +0200

    change notation upto 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,7 +7,7 @@ class Review < ApplicationRecord
     length: { in: 30..1000 }
   validates :notation,
     presence: true,
-    inclusion: { in: 1..6},
+    inclusion: { in: 1..5},
     format: { with: /\A\d+\.*\d{0,1}\z/ }
 
   belongs_to :internship
