784cca219ef13ade584b99540c74ff3e197f722c
Heroponne <florence.orgeret@gmail.com>
2020-04-10 11:34:25 +0200

    [BACK] edited redirection of destroy method

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -57,7 +57,7 @@ class FavoritesController < ApplicationController
   def destroy
     @favorite.destroy
     respond_to do |format|
-      format.html { redirect_to favorites_url, notice: 'Favorite was successfully destroyed.' }
+      format.html { redirect_back fallback_location: root_path, notice: 'Favorite was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
