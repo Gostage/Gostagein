096505453ebeaec9f6827edd36c15900ad2fb55f
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-04-16 13:27:53 +0200

    BACK add specific routes for sign out because error

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
   resources :internships
   root to: "static_pages#home"
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # To fix the error that occured when user sign out
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :users, only: [:show]
 end
