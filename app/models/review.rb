0981dda06f12b76e70c2ecfd1572c458e5c12881
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-04-20 16:09:01 +0200

    [done] BACK validates belongs_to

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,7 +10,7 @@ class Review < ApplicationRecord
     inclusion: { in: 1..5},
     format: { with: /\A\d+\.*\d{0,1}\z/ }
 
-  belongs_to :internship
-  belongs_to :user
+  belongs_to :review_user, class_name: "User"
+  belongs_to :review_internship, class_name: "Internship"
 
 end
