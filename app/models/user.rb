51662dc8ea6bf5956ed15109f94801f32838a2fa
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-04-20 16:09:28 +0200

    [done] BACK validates review_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,9 @@ class User < ApplicationRecord
   
   has_many :internships
   has_many :comments
-  has_many :reviews
+
+  has_many :review_users, foreign_key: 'review_user_id', class_name: "User"
+
   has_many :favorites, foreign_key: "favorite_user_id"
   has_many :favorite_internships, foreign_key: "favorite_internship_id", class_name: "Internship", through: :favorites
 
