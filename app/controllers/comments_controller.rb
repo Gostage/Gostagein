49c574c7ae9e0faf8d985600d7a748b205604c8c
Heroponne <florence.orgeret@gmail.com>
2020-05-10 17:19:42 +0200

    [BACK] edited methods for nested routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :get_questioned_internship
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :current_user_must_own_comment, only: [:edit, :update, :destroy]
 
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = @questioned_internship.comments
   end
 
   # GET /comments/1
@@ -15,22 +17,22 @@ class CommentsController < ApplicationController
 
   # GET /comments/new
   def new
-    @comment = Comment.new
+    @comment = @questioned_internship.comments.build
   end
 
   # GET /comments/1/edit
   def edit
-    current_user_must_own_comment
   end
 
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @questioned_internship.comments.build(comment_params)
+    @comment.questioner = current_user
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to internship_comments_path(@questioned_internship), notice: 'Le commentaire a été posté.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
@@ -44,7 +46,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to internship_comments_path(@questioned_internship), notice: 'Le commentaire a été modifié.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
@@ -58,7 +60,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to internship_comments_path(@questioned_internship), notice: 'Le commentaire a été supprimé.' }
       format.json { head :no_content }
     end
   end
@@ -66,18 +68,23 @@ class CommentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
-      @comment = Comment.find(params[:id])
+      @comment = @questioned_internship.comments.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:content, :questioner_id, :questioned_internship_id)
     end
 
     # Editing permission only for owner
     def current_user_must_own_comment
-      if current_user.id != @comment.user.id
-        redirect_to comment_path(@comment.id)
+      if current_user.id != @comment.questioner.id
+        redirect_to internship_comments_path(@questioned_internship)
       end
     end
+
+    def get_questioned_internship
+      @questioned_internship = Internship.find(params[:internship_id])
+    end
+
 end
