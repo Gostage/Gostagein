50fa85a76478a7219bf87208ad231ed349f97360
Heroponne <florence.orgeret@gmail.com>
2020-05-10 17:24:17 +0200

    [BACK] edited relations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
   validates :content,
     presence: true,
+    uniqueness: true,
     length: { maximum: 300 }
-  belongs_to :user
-  belongs_to :internship
+  belongs_to :questioner, class_name: "User"
+  belongs_to :questioned_internship, class_name: "Internship"
 end
