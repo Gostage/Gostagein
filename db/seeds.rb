ad3599db8e218375a7caab8482ff51348d246b5c
Heroponne <florence.orgeret@gmail.com>
2020-05-16 13:21:49 +0200

    [BACK] edited number of users created

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.destroy_all
 
 require 'faker'
 
-100.times do |user|
+30.times do |user|
 	user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "azerty")
 end
 
