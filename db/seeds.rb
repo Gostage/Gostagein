c88252b3499d991aeb583fa30a6033b4f399de5e
Heroponne <florence.orgeret@gmail.com>
2020-06-05 09:36:43 +0200

    [BACK] layout and message changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,17 @@ User.destroy_all
 require 'faker'
 
 30.times do |user|
-	user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "azerty")
+	User.create(first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name, 
+    email: Faker::Internet.email, 
+    password: "azerty")
 end
 
-puts "100 utilisateurs ont été crées"
+puts "30 utilisateurs ont été crées"
 
 
 100.times do |internship|
-	internship = Internship.create(adress: Faker::Address.street_address, 
+	Internship.create(adress: Faker::Address.street_address, 
 		zipcode: Faker::Address.zip_code, 
 		city: Faker::Address.city, 
 		specialty: ["Hospitalier", "Maison médicale","Social", "Médico-social", "Petite enfance", "Associatif", "Scolaire", "Médico-pédagogique", "Libéral", "Recherche", "Entreprise","Privé", "Justice","Police Nationale", "Armée", "Carcéral", "Insertion professionnelle", "Recrutement", "Gestion de carrières", "Ressouces Humaines", "Marketing", "Santé au travail", "Autres"].sample, 
@@ -25,6 +28,8 @@ puts "100 utilisateurs ont été crées"
 		user_id: User.all.sample.id, 
 		description: Faker::GreekPhilosophers.quote, 
 		title: Faker::Job.title, 
+		feeling: Faker::GreekPhilosophers.quote,
+		notation: [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5].sample,
 		cursus: ["Licence 1", "Licence 2", "Licence 3", "Master 1", "Master 2"].sample,
 		region: ["Auvergne-Rhône-Alpes", "Bourgogne-Franche-Comté", "Bretagne", "Centre-Val de Loire", "Corse", "Grand Est", "Hauts-de-France", "Île-de-France", "Normandie", "Nouvelle-Aquitaine", "Occitanie", "Pays de la Loire", "Provence-Alpes-Côte d'Azur"].sample)
 end
@@ -33,24 +38,29 @@ puts "100 stages ont été crées"
 
 
 100.times do |favorite|
-	favorite = Favorite.create(favorite_user_id: User.all.sample.id, favorite_internship_id: Internship.all.sample.id)
+	Favorite.create(favorite_user_id: User.all.sample.id, 
+    favorite_internship_id: Internship.all.sample.id)
 end
 
 puts "100 favoris ont été crées"
 
 100.times do |comment|
-	Comment.create(content: Faker::Quote.most_interesting_man_in_the_world, questioner_id: User.all.sample.id,
-		commentable_id: Internship.all.sample.id, commentable_type: "Internship")
-	Comment.create(content: Faker::Quote.most_interesting_man_in_the_world, questioner_id: User.all.sample.id,
-		commentable_id: Comment.all.sample.id, commentable_type: "Comment")
+	Comment.create(content: Faker::Quote.most_interesting_man_in_the_world, 
+    questioner_id: User.all.sample.id,
+		commentable_id: Internship.all.sample.id, 
+    commentable_type: "Internship")
+	Comment.create(content: Faker::Quote.most_interesting_man_in_the_world, 
+    questioner_id: User.all.sample.id,
+		commentable_id: Comment.all.sample.id, 
+    commentable_type: "Comment")
 end
 
 puts "200 commentaires ont été crées"
 
 100.times do |review|
-	review = Review.create(title: Faker::Lorem.sentence,
+	Review.create(title: Faker::Lorem.sentence,
 		description: Faker::Lorem.paragraph,
-		notation: [1, 2, 2.5, 3, 3.5, 4, 4.5, 5].sample,
+		notation: [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5].sample,
 		review_user_id: User.all.sample.id,
 		review_internship_id: Internship.all.sample.id)
 end
