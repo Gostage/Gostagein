09c2778f93c74cc3e3b822c49fd898b191e42b9a
Heroponne <florence.orgeret@gmail.com>
2020-06-08 21:43:33 +0200

    [BACK] edit show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @internships = @user.internships
-    @comments = @user.comments.to_a.uniq
+    @comments = @user.comments
     @reviews = @user.reviews
   end
 
