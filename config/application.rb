095c5a24c3ec197897b2ddfebd1fe781a2b9f729
Heroponne <florence.orgeret@gmail.com>
2020-06-10 21:30:15 +0200

    [BACK] added devise french file and make the linking in application.rb in config.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module Gostagein
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # devise messages in french
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.available_locales = [:fr, :en]
+    config.i18n.default_locale = :fr
   end
 end
