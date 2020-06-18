91de03134a1a86c4d5f952e744f9501872ecf71d
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-06-18 20:48:33 +0200

    [done] action mail smtp

--- config/environment.rb
+++ config/environment.rb
@@ -9,3 +9,12 @@ Rails.application.initialize!
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   html_tag.html_safe
 end
+
+ActionMailer::Base.smtp_settings =   {
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com', #you can also use google.com
+    :authentication     => :plain,
+    :user_name          => ENV['GMAIL_LOGIN'],
+    :password           => ENV['GMAIL_PWD']
+  }
